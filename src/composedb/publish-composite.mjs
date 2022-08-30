2827fd763e170bc88d78265a711ca28c60bf2c6d
Cody Hatfield <cody.hatfield@me.com>
2022-08-30 10:47:31 -0700

    Initial ComposeDB schema

---
+++ src/composedb/publish-composite.mjs
@@ -0,0 +1,33 @@
+import { CeramicClient } from "@ceramicnetwork/http-client";
+import { DID } from "dids";
+import { Ed25519Provider } from "key-did-provider-ed25519";
+import { getResolver } from "key-did-resolver";
+import { fromString } from "uint8arrays/from-string";
+import {
+  createComposite,
+  writeEncodedComposite,
+} from "@composedb/devtools-node";
+
+const ceramic = new CeramicClient("https://ceramic-clay.3boxlabs.com");
+
+// `seed` must be a 32-byte long Uint8Array
+async function authenticateCeramic(seed) {
+  const provider = new Ed25519Provider(seed);
+  const did = new DID({ provider, resolver: getResolver() });
+  // Authenticate the DID with the provider
+  await did.authenticate();
+  // The Ceramic client can create and update streams using the authenticated DID
+  ceramic.did = did;
+}
+
+async function run() {
+  const seed = fromString(process.env.CERAMIC_SEED, "base16");
+  await authenticateCeramic(seed);
+
+  const composite = await createComposite(ceramic, "schema.graphql");
+
+  // Replace by the path to the encoded composite file
+  await writeEncodedComposite(composite, "composite.json");
+}
+
+run();
